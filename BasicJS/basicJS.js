125a1862bbc35afa499f88ee4187fd26b979958d
salma-j-tech <sa726273@ucf.edu>
2026-02-16 16:50:39 -0500

    Create functions for the counter

--- BasicJS/basicJS.js
+++ BasicJS/basicJS.js
@@ -1,1 +1,29 @@
+// funcs for the counter
+
+function counter() { // start counter
+
+  return parseInt (document.getElementById("counter").innerText, 10);
+  
+} // end counter
+
+function updateCounter(value) {
+
+  document.getElementById("counter").innerText = value;
+  
+} // end updateCounter func
+
+function tickUp() { // counter + 1
+
+  updateCounter(counter() + 1);
+  
+} // end tickUp
+
+function tickDown() { // counter - 1
+
+  updateCounter(counter() - 1);
+  
+} // end tickDown
+
+
+
 
