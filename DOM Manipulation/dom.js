19f807e04639f0be0126eb11f6819a307af94a67
salma-j-tech <sa726273@ucf.edu>
2026-02-18 16:01:38 -0500

    creating dom manipulation js file

---
+++ DOM Manipulation/dom.js
@@ -0,0 +1,16 @@
+// func to run after page loaded
+document.addEventListener("contentLoaded", function () {
+  
+  document.getElementById("filterContent").style.display = "none";
+  document.getElementById("newContent").style.display = "none";
+
+  filerArticles();
+  
+}) // end contentLoaded func
+
+// func to display filter
+function displayFilter() {
+
+  const filterForm = document.getElementById("filterContent");
+  
+} // end displayFilter
