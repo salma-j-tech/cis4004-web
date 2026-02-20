21a1d014e98a48af38e3b81cd6b1e82a9a228e8a
salma-j-tech <sa726273@ucf.edu>
2026-02-19 23:12:56 -0500

    Added filterArticles function to js file

--- DOM Manipulation/dom.js
+++ DOM Manipulation/dom.js
@@ -54,4 +54,47 @@ function showAddNew() {
 } // end showAddNew func
 
 
+// func for visibility based on boxes checked
+function filterArticles() {
+
+  // check which filters are live
+  let opinionChecked = document.getElementById("opinionCheckbox").checked;
+  let recipeChecked = document.getElementById("recipeCheckbox").checked;
+  let updateChecked = document.getElementById("updateCheckbox").checked;
+
+  let articles = document.getElementById("articleList")
+  {
+    .getElementsByTagName("article");
+  } // end let
+
+  // counter for loop
+  for (let i = 0; i < articles.length; i++)
+  {
+
+    // check if match article classes
+    if (articles[i].classList.contains("opinion"))
+    {
+
+      articles[i].style.display = opinionChecked ? "" : "none";
+      
+    } // end if
+
+    if (articles[i].classList.contains("recipe"))
+    {
+
+      articles[i].style.display = recipeChecked ? "" : "none";
+      
+    } // end if
+
+    if (articles[i].classList.contains("update"))
+    {
+
+      articles[i].style.display = updateChecked ? "" : "none";
+      
+    } // end if
+    
+    
+  } // end for
+  
+} // end filterArticles
 
